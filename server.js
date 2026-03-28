76dba50b21aa6ee35f455314d822f08cd537ee9f
Vivek-Jayaswal <vivekkumarjaiswal2000@gmail.com>
2026-03-29 00:48:29 +0530

    cors added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const app = express();
+const cors = require("cors");;
 
 // file import
 const db = require("./db");
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
+app.use(cors())
 
 app.use("/auth", authRouter);
 app.use("/product", productRouter);
