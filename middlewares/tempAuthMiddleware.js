57a91b69b598ee9c0467446780ebd9d836431177
Vivek-Jayaswal <vivekkumarjaiswal2000@gmail.com>
2026-04-09 22:30:24 +0530

    done

--- middlewares/tempAuthMiddleware.js
+++ middlewares/tempAuthMiddleware.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const tempAuthMiddleware = (req, res, next) => {
   const token = req.headers?.authorization?.split(" ")[1];
+  console.log(token);
   if (!token) {
     return res.status(401).json({
       message: "No Token Provided!",
